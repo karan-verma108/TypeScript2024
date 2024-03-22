b3a734f08552d6162a49240f7bc5f9f7300e4c2a
karan <karanverma201411@gmail.com>
2024-03-22 21:52:46 +0530

    first

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+// let a:number = 5;
+Object.defineProperty(exports, "__esModule", { value: true });
+// console.log(a);
+const addition = (a, b) => {
+    return a + b;
+};
+let result = addition(4, 3);
+console.log(result);
---
+++ index.ts
@@ -0,0 +1,12 @@
+// let a:number = 5;
+
+// console.log(a);
+
+const addition = (a:number, b:number):number=>{
+    return a+b;
+}
+
+let result = addition(4,3);
+console.log(result);
+
+export {}
